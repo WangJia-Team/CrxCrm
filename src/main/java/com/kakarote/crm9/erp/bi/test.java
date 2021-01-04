ee22c8422b4d5a0c47ce95b35f858eca930ee390
19307 <wangdiwrg@mail.com>
2021-01-04 16:42:29 +0800

    王迪 商业智能

--- src/main/java/com/kakarote/crm9/erp/bi/test.java
+++ src/main/java/com/kakarote/crm9/erp/bi/test.java
@@ -2,6 +2,6 @@ package com.kakarote.crm9.erp.bi;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("helzdzcsdvlo world");
+        System.out.println("helzdzcksdvlo world");
     }
 }
