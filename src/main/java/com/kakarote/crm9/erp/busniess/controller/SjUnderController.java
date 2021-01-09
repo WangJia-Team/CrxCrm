ebb92369cc5081b68b20bac96b66b34a801e878f
wq4966 <paxl4966@163.com>
2021-01-09 17:28:16 +0800

    1122

--- src/main/java/com/kakarote/crm9/erp/busniess/controller/SjUnderController.java
+++ src/main/java/com/kakarote/crm9/erp/busniess/controller/SjUnderController.java
@@ -5,10 +5,12 @@ import com.kakarote.crm9.erp.busniess.model.Business;
 import com.kakarote.crm9.erp.busniess.service.UnderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 public class SjUnderController {
@@ -18,16 +20,16 @@ public class SjUnderController {
 
 
     @RequestMapping("sjview.do")
-    @ResponseBody
-    public String view(Business business, HttpServletRequest request) {
-
-        return service.view(business,request);
+    public String view(Business business, HttpServletRequest request, Model  model) {
+       List<Business> list =  service.view(business,request);
+       model.addAttribute("list",list);
+        return "shangji/sjview";
     }
 
 
     @RequestMapping("save.do")
     @ResponseBody
-    public Message bulid(HttpServletRequest request,Business  business){
+    public Message save(HttpServletRequest request,Business  business){
 
         return service.save(business,request);
     }
